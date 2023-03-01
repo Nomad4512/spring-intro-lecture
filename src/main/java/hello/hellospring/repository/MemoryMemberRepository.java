4e53e5b512ef45c0e70abd377324f16dc9963d0e
hyunbin <zizonbini@gmail.com>
2023-03-01 18:36:49 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();// 실무에서는 동시성문제 있을 수있어서 ConcurrentHashMap 사용
